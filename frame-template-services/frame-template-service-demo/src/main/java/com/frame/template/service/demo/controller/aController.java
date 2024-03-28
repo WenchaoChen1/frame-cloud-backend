3107065e70780a4f80b0be8fd40478abad5bf6b9
wenchao <wenchao@gstdev.com>
2024-03-28 17:59:26 +0800

    n m

--- frame-template-services/frame-template-service-demo/src/main/java/com/frame/template/service/demo/controller/aController.java
+++ frame-template-services/frame-template-service-demo/src/main/java/com/frame/template/service/demo/controller/aController.java
@@ -1,6 +1,8 @@
 package com.frame.template.service.demo.controller;
 
 import com.frame.template.common.utils.SecurityUtils;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +19,22 @@ public class aController {
   }
   @GetMapping("/bb")
   public String testa() {
+    Object userDetails = SecurityUtils.getUserDetails();
     System.out.println("aaaaaaaaaaa");
 //    ServletUtils.getRequest()
     return "System.out.println(SecurityUtils.getUserDetails());";
   }
+  /**
+   * 获取用户信息
+   * @return
+   */
+  @GetMapping("/user")
+  public Authentication oauth2UserInfo(){
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if(authentication == null){
+      throw new RuntimeException("无有效认证用户！");
+    }
+    return authentication;
+  }
+
 }
