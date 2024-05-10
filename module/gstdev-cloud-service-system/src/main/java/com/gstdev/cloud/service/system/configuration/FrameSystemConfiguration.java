c1233cdc4b96cb8795f8b26418d78e26d92967d2
wenchao <wenchao@goldensection.com>
2024-05-10 17:29:27 +0800

    no message

--- module/gstdev-cloud-service-system/src/main/java/com/gstdev/cloud/service/system/configuration/FrameSystemConfiguration.java
+++ module/gstdev-cloud-service-system/src/main/java/com/gstdev/cloud/service/system/configuration/FrameSystemConfiguration.java
@@ -1,17 +1,10 @@
 
 package com.gstdev.cloud.service.system.configuration;
 
-import com.gstdev.cloud.service.system.mapper.*;
-import com.gstdev.cloud.service.system.repository.*;
-import com.gstdev.cloud.service.system.service.*;
 import jakarta.annotation.PostConstruct;
-import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
