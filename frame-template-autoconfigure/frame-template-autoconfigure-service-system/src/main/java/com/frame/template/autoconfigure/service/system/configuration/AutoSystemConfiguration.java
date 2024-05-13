55b28cba1b47729842cbbdc643c49f76c23567c9
wenchao <wenchao@goldensection.com>
2024-05-13 17:46:36 +0800

    no message

--- frame-template-autoconfigure/frame-template-autoconfigure-service-system/src/main/java/com/frame/template/autoconfigure/service/system/configuration/AutoSystemConfiguration.java
+++ frame-template-autoconfigure/frame-template-autoconfigure-service-system/src/main/java/com/frame/template/autoconfigure/service/system/configuration/AutoSystemConfiguration.java
@@ -2,6 +2,7 @@ package com.frame.template.autoconfigure.service.system.configuration;
 
 
 import com.gstdev.cloud.service.system.configuration.FrameSystemConfiguration;
+import com.gstdev.cloud.starter.ouath2.resource.server.configuration.ResourceServerAutoConfiguration;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,10 +22,24 @@ import org.springframework.context.annotation.Import;
     "com.frame.template.autoconfigure.service.system.feign.service",
     "com.frame.template.autoconfigure.service.system.listener",
     "com.frame.template.autoconfigure.service.system.processor",
-    "com.gstdev.cloud",
+//    "com.gstdev.cloud",
+//    "com.gstdev.cloud.service.system",
+//    "com.gstdev.cloud.springframework.openfeign",
+//    "com.gstdev.cloud.cache.jetcache",
+//    "com.gstdev.cloud.rest.autoconfigure",
+//    "com.gstdev.cloud.web",
+//    "com.gstdev.cloud.starter",
+//    "com.gstdev.cloud.base",
+//    "com.gstdev.cloud.base.core.json.jackson2.utils",
+//    "com.gstdev.cloud.cache",
+//    "com.gstdev.cloud.message",
+//    "com.gstdev.cloud.rest",
+//    "com.gstdev.cloud.captcha",
+//    "com.gstdev.cloud.data",
+//    "com.gstdev.cloud.oauth2"
 })
 @EntityScan(value = {"com.frame.template.service.system"})
-@Import({FrameSystemConfiguration.class})
+@Import({FrameSystemConfiguration.class, ResourceServerAutoConfiguration.class})
 public class AutoSystemConfiguration {
 
     private static final Logger log = LoggerFactory.getLogger(AutoSystemConfiguration.class);
