c84edbb3ced8aacdad6a0ff0cbed5825aa65000b
wenchao <wenchao@goldensection.com>
2024-05-13 17:49:44 +0800

    no message

--- frame-template-services/frame-template-service-identity/src/main/java/com/frame/template/service/identity/IdentityApplication.java
+++ frame-template-services/frame-template-service-identity/src/main/java/com/frame/template/service/identity/IdentityApplication.java
@@ -9,27 +9,12 @@
 
 package com.frame.template.service.identity;
 
-
 import com.frame.template.service.identity.configuration.IdentityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@EnableFeignClients
-//@ComponentScan(value = {
-//    "com.frame.template.common.redis.currentLoginInformation",
-//    "com.frame.template",
-//    "com.gstdev.cloud.rest.autoconfigure",
-//    "com.gstdev.cloud",
-//})
-//@EntityScan(value = {"com.frame.template.service.identity"})
-//@EnableJpaRepositories(value = {"com.frame.template.service.identity"})
 @SpringBootApplication
-//@EnableFrameJetCache
 
 @Import(IdentityConfiguration.class)
 public class IdentityApplication {
--- frame-template-services/frame-template-service-system/src/main/java/com/frame/template/service/system/SystemApplication.java
+++ frame-template-services/frame-template-service-system/src/main/java/com/frame/template/service/system/SystemApplication.java
@@ -10,28 +10,11 @@
 package com.frame.template.service.system;
 
 import com.frame.template.autoconfigure.service.system.configuration.AutoSystemConfiguration;
-import com.gstdev.cloud.cache.jetcache.annotation.EnableFrameJetCache;
-import com.gstdev.cloud.service.system.configuration.FrameSystemConfiguration;
-import com.gstdev.cloud.starter.ouath2.resource.server.configuration.ResourceServerAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@EnableFeignClients
-//@ComponentScan(value = {
-//    "com.frame.template.service.system",
-//    "com.frame.template.common.redis.currentLoginInformation",
-//    "com.gstdev.cloud.rest.autoconfigure",
-//    "com.gstdev.cloud",
-//})
-//@EntityScan(value = {"com.frame.template.service.system"})
 @SpringBootApplication
-//@EnableFrameJetCache
 @Import({AutoSystemConfiguration.class})
 public class SystemApplication {
 
