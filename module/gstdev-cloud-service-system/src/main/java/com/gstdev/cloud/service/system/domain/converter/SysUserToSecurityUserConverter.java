2c94da1b8f197e941a52d3152c7d3771be69a4e7
wenchao <wenchao@goldensection.com>
2024-06-27 15:40:48 +0800

    no message

--- module/gstdev-cloud-service-system/src/main/java/com/gstdev/cloud/service/system/domain/converter/SysUserToSecurityUserConverter.java
+++ module/gstdev-cloud-service-system/src/main/java/com/gstdev/cloud/service/system/domain/converter/SysUserToSecurityUserConverter.java
@@ -3,21 +3,15 @@ package com.gstdev.cloud.service.system.domain.converter;
 import com.gstdev.cloud.data.core.enums.DataItemStatus;
 import com.gstdev.cloud.oauth2.core.definition.domain.DefaultSecurityUser;
 import com.gstdev.cloud.oauth2.core.definition.domain.FrameGrantedAuthority;
-import com.gstdev.cloud.oauth2.core.utils.SecurityUtils;
 import com.gstdev.cloud.service.system.domain.entity.SysAccount;
-import com.gstdev.cloud.service.system.domain.entity.SysPermission;
-import com.gstdev.cloud.service.system.domain.entity.SysRole;
 import com.gstdev.cloud.service.system.domain.entity.SysUser;
-import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.core.convert.converter.Converter;
 
 import java.time.Instant;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * <p>Description: SysUser 转 SecurityUser 转换器 </p>
@@ -31,23 +25,23 @@ public class SysUserToSecurityUserConverter implements Converter<SysUser, Defaul
 
         Set<String> roles = new HashSet<>();
 
-        List<SysRole> accountRoles = sysUser.getAccount().stream()
-                .flatMap(account -> account.getRoles().stream())
-                .collect(Collectors.toList());
+//        List<SysRole> accountRoles = sysUser.getAccount().stream()
+//                .flatMap(account -> account.getRoles().stream())
+//                .collect(Collectors.toList());
 //        for (SysAccount sysAccount : sysUser.getAccount()) {
 //            if (sysAccount.getType().equals(SysAccountType.SUPER)) {
 //                authorities.add(new FrameGrantedAuthority("5ef5ef0364b6939c4ca61f34b393f7b368d1be8619647aaf83d5b395919ab629"));
 //            }
 //        }
 
-        for (SysRole sysRole : accountRoles) {
-            roles.add(sysRole.getCode());
-            authorities.add(new FrameGrantedAuthority(SecurityUtils.wellFormRolePrefix(sysRole.getCode())));
-            Set<SysPermission> sysPermissions = sysRole.getPermissions();
-            if (CollectionUtils.isNotEmpty(sysPermissions)) {
-                sysPermissions.forEach(sysAuthority -> authorities.add(new FrameGrantedAuthority((sysAuthority.getPermissionCode()))));
-            }
-        }
+//        for (SysRole sysRole : accountRoles) {
+//            roles.add(sysRole.getCode());
+//            authorities.add(new FrameGrantedAuthority(SecurityUtils.wellFormRolePrefix(sysRole.getCode())));
+//            Set<SysPermission> sysPermissions = sysRole.getPermissions();
+//            if (CollectionUtils.isNotEmpty(sysPermissions)) {
+//                sysPermissions.forEach(sysAuthority -> authorities.add(new FrameGrantedAuthority((sysAuthority.getPermissionCode()))));
+//            }
+//        }
         Optional<SysAccount> firstAccount = sysUser.getAccount().stream().findFirst();
         DefaultSecurityUser defaultSecurityUser = new DefaultSecurityUser(
                 sysUser.getId(),
