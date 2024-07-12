2fddcd64351ea29762bb151d9283dd47b154ea6f
wenchao <wenchao@goldensection.com>
2024-07-12 10:50:47 +0800

    no message

--- frame-template-services/frame-template-service-storage/src/main/java/com/frame/template/service/storage/controller/FileController.java
+++ frame-template-services/frame-template-service-storage/src/main/java/com/frame/template/service/storage/controller/FileController.java
@@ -1,26 +1,25 @@
-//// ====================================================
+////====================================================
 ////
-//// This file is part of the GstDev Cloud Platform.
+////This file is part of the GstDev Cloud Platform.
 ////
-//// Create by gstdev Tech <support@gstdev.com>
-//// Copyright (c) 2020-2025 gstdev.com
+////Create by gstdev Tech <support@gstdev.com>
+////Copyright (c) 2020-2025 gstdev.com
 ////
-//// ====================================================
+////====================================================
 //
 //package com.frame.template.service.storage.controller;
 //
+//import com.frame.template.common.constant.FileConstants;
 //import com.frame.template.service.storage.domain.base.*;
 //import com.frame.template.service.storage.domain.entity.File;
 //import com.frame.template.service.storage.mapper.vo.FileVoMapper;
 //import com.frame.template.service.storage.service.FileService;
 //import com.gstdev.cloud.base.definition.domain.Result;
-//import com.frame.template.common.constant.FileConstants;
 //import com.gstdev.cloud.rest.core.controller.BasePOJOController;
 //import io.swagger.v3.oas.annotations.Operation;
+//import jakarta.annotation.Resource;
 //import org.springframework.web.bind.annotation.*;
 //import org.springframework.web.multipart.MultipartFile;
-//
-//import jakarta.annotation.Resource;
 //
 //import java.io.IOException;
 //import java.util.List;
@@ -30,6 +29,7 @@
 //@RequestMapping("/v1/file")
 //public class FileController extends BasePOJOController<File, String, FileService, FileVoMapper, FileVo, FileDto, FileInsertInput, FileUpdateInput, FilePageQueryCriteria, FileFindAllByQueryCriteria> {
 //
+//
 //    @Resource
 //    private FileService fileService;
 //
@@ -38,30 +38,30 @@
 //
 //    public FileController(FileService fileService, FileVoMapper fileVoMapper) {
 //        super(fileService, fileVoMapper);
-////        this.fileService = fileService;
-////        this.fileVoMapper = fileVoMapper;
+//        this.fileService = fileService;
+//        this.fileVoMapper = fileVoMapper;
 //    }
 //
 //    @GetMapping("/get-by-id")
-@Operation(summary = "根据id获取实体数据")
+//    @Operation(summary = "根据id获取实体数据")
 //    public Result<FileVo> getById(String id) {
 //        return findByIdToResult(id);
 //    }
 //
 //    @PostMapping
-@Operation(summary = "新增一条数据")
+//    @Operation(summary = "新增一条数据")
 //    public Result<FileVo> insert(FileInsertInput fileInsertInput, @RequestParam("file") MultipartFile file) {
 //        return insertToResult(fileInsertInput);
 //    }
 //
 //
-@Operation(summary = "Delete file")
+//    @Operation(summary = "Delete file")
 //    @DeleteMapping
 //    public Result<String> delete(@PathVariable final String id) {
 //        return deleteByIdToResult(id);
 //    }
 //
-@Operation(summary = "Upload file")
+//    @Operation(summary = "Upload file")
 //    @PostMapping(value = "/upload")
 //    public Result<FileVo> upload(@RequestParam("file") MultipartFile file, @RequestParam("tenantId") String tenantId, @RequestParam("type") FileConstants fileConstants) throws IOException {
 //        if (fileConstants == null) {
@@ -72,7 +72,7 @@
 //
 //    }
 //
-@Operation(summary = "Uploads file")
+//    @Operation(summary = "Uploads file")
 //    @PostMapping(value = "/uploads")
 //    public Result<List<FileVo>> uploads(@RequestParam("file") List<MultipartFile> file, @RequestParam("tenantId") String tenantId, @RequestParam("type") FileConstants fileConstants) throws IOException {
 //        Result<List<FileVo>> fileVoResult = getMapper().toAllVo(fileService.uploads(file, tenantId, fileConstants));
