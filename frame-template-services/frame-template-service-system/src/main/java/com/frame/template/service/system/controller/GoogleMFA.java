d28af3da092c992254116757ba869632861ce633
wenchao <wenchao@goldensection.com>
2024-08-19 10:10:53 +0800

    no message

--- frame-template-services/frame-template-service-system/src/main/java/com/frame/template/service/system/controller/GoogleMFA.java
+++ frame-template-services/frame-template-service-system/src/main/java/com/frame/template/service/system/controller/GoogleMFA.java
@@ -25,30 +25,25 @@ public class GoogleMFA {
 
         // 生成当前的TOTP代码
 //        int code = gAuth.getTotpPassword(secret);
-        int code = gAuth.getTotpPassword("QVKRRGVJ4CNXQYKJOCZKBLUELYSBBTML");
+        int code = gAuth.getTotpPassword("BRF23ZDOFIBUT6ZCR4EYCQS2ONZJ7HO4");
         System.out.println("Current OTP: " + code);
 
         // 验证TOTP代码
-        boolean isCodeValid = gAuth.authorize("QVKRRGVJ4CNXQYKJOCZKBLUELYSBBTML", code);
+        boolean isCodeValid = gAuth.authorize("BRF23ZDOFIBUT6ZCR4EYCQS2ONZJ7HO4", code);
         System.out.println("Is code valid? " + isCodeValid);
 
 //        System.out.println("Current OTP: " + generateQRCodeBase64("ALBAGBDBKPWUQUGL"));
 //        System.out.println("Current OTP: " + generateQRCodeBase64("otpauth%3A%2F%2Ftotp%2FMyApp%3Auser%40example.com%3Fsecret%3D6XCO4T77RYHYC62T%26issuer%3DMyApp%26algorithm%3DSHA1%26digits%3D6%26period%3D30&size=200x200&ecc=M&margin=0"));
+        String totpUri = "otpauth://totp/MyApp:user@example.com?secret=BRF23ZDOFIBUT6ZCR4EYCQS2ONZJ7HO4&issuer=MyApp&algorithm=SHA1&digits=6&period=30";
+        int size = 200;
 
+        try {
+            String base64QRCode = GoogleAuthenticatorQRGenerator.getOtpAuthTotpURLBase64(totpUri, size,size);
+            System.out.println("Base64 QR code: " + base64QRCode);
+        } catch (Exception e) {
+            System.err.println("Could not generate QR code: " + e.getMessage());
+        }
     }
-//        /**
-//         * 生成二维码
-//         */
-//        public static String generateQRCodeBase64(String content) throws WriterException, IOException {
-//            // 生成二维码
-//            BitMatrix bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, 300, 300);
-//
-//            // 将二维码写入字节流
-//            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-//            MatrixToImageWriter.writeToStream(bitMatrix, "png", outputStream);
-//
-//            // 转换为 Base64 编码字符串
-//            byte[] qrCodeBytes = outputStream.toByteArray();
-//            return Base64.getEncoder().encodeToString(qrCodeBytes);
-//        }
+
+
 }
