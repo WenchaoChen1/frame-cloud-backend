142a39198d55c9ffaeced495ceef5ec1a96ff1cc
wenchao <wenchao@goldensection.com>
2025-01-05 17:14:22 +0800

    demo

---
+++ frame-template-services/frame-template-service-system/src/main/java/com/frame/template/service/system/controller/c.java
@@ -0,0 +1,107 @@
+package com.frame.template.service.system.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class c {
+
+    /**
+     * 2
+     * 1 2 12
+     * <p>
+     * 3
+     * 1 2 3 12 13 23 123
+     * <p>
+     * 4
+     * 1 2 3 4    12 13 14 23 24 34   123 124 134 234    1234
+     * <p>
+     * 5
+     * 1 2 3 4 5 12 13 14 15 23 24 25 34 35 45  123 124 125 134 135 145 234 235 245 345  1234 1235 1245 1345 2345 12345
+     * <p>
+     * 6
+     * 1 2 3 4 5 6 12 13 14 15 16 23 24 25 26 34 35 36 45 46 56 123 124 125 126 134 135 136 145 146 156 234 235 236 245 246 256 345 346 356 456 1234 1235 1236 1245 1246 1256 1345 1346 1356 1456 2345 2346 2356 2456 3456 12345 12346 12356 12456 13456 23456 123456
+     */
+    // 定义 User 对象
+    static class User {
+        String name;
+
+        public User(String name) {
+            this.name = name;
+        }
+    }
+
+
+//    public static List<String> printCombinations(List<String> array, int start, List<List<String>> prefix, List<String> all) {
+//        if (array == null || start >= array.size()) {
+//            return all;
+//        }
+//
+//        if (prefix.isEmpty()) {
+//            return all;
+//        }
+//
+//        List<List<String>> objects = new ArrayList<>();
+//        for (int i = 0; i < prefix.size(); i++) {
+//            List<String> currentCombos = new ArrayList<>();
+//            for (String string1 : prefix.get(i)) {
+//                for (int a = start + i + 1; a < array.size(); a++) {
+//                    String string = array.get(a);
+//                    if (!string1.contains(string)) {
+//                        StringBuilder sb = new StringBuilder();
+//                        sb.append(string1).append(string);
+//                        currentCombos.add(sb.toString());
+//                    }
+//                }
+//            }
+//            if (!currentCombos.isEmpty()) {
+//                objects.add(currentCombos);
+//            }
+//        }
+//
+//        System.out.println(objects);
+//        List<String> strings = printCombinations(array, start + 1, objects, all);
+//        return strings;
+//    }
+//
+//    public static void main(String[] args) {
+//        List<List<String>> combinedCodes = new ArrayList<>();
+//        List<String> a = new ArrayList<>();
+//        List<String> all = new ArrayList<>();
+//        for (int i = 0; i < 100; i++) {
+//            a.add(String.valueOf(i + 1));
+//            List<String> b = new ArrayList<>();
+//            b.add(String.valueOf(i + 1));
+//            combinedCodes.add(b);
+//        }
+//        List<String> strings = printCombinations(a, 0, combinedCodes, all);
+//        System.out.println(strings);
+//    }
+
+    // 递归生成组合
+    public static void generateCombinations(List<String> array, int start, String prefix, List<String> all) {
+        // 将当前组合添加到结果中（跳过空前缀）
+        if (!prefix.isEmpty()) {
+            all.add(prefix);
+        }
+
+        // 遍历剩余的元素，生成新的组合
+        for (int i = start; i < array.size(); i++) {
+            generateCombinations(array, i + 1, prefix + array.get(i), all);
+        }
+    }
+
+    public static void main(String[] args) {
+        // 初始化输入数据
+        List<String> input = new ArrayList<>();
+        for (int i = 1; i <= 50; i++) {  // 修改这里的上限值，控制生成的组合
+            input.add(String.valueOf(i));
+        }
+
+        // 存储所有的组合
+        List<String> allCombinations = new ArrayList<>();
+        generateCombinations(input, 0, "", allCombinations);
+
+        // 输出结果
+        System.out.println(String.join(" ", allCombinations));
+    }
+}
