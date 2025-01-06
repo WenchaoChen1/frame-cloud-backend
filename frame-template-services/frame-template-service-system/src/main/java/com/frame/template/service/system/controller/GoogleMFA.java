62f52d0adeea94b82de54187e272656df8ceb948
wenchao <wenchao@goldensection.com>
2025-01-06 10:57:40 +0800

    解决sonarqube issues/解决pom多个重复引用问题

--- frame-template-services/frame-template-service-system/src/main/java/com/frame/template/service/system/controller/GoogleMFA.java
+++ frame-template-services/frame-template-service-system/src/main/java/com/frame/template/service/system/controller/GoogleMFA.java
@@ -1,49 +1,49 @@
-package com.frame.template.service.system.controller;
-
-
-import com.gstdev.cloud.authenticator.qr.google.GoogleAuthenticator;
-import com.gstdev.cloud.authenticator.qr.google.GoogleAuthenticatorKey;
-import com.gstdev.cloud.authenticator.qr.google.GoogleAuthenticatorQRGenerator;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/mfa")
-public class GoogleMFA {
-
-
-    public static void main(String[] args) throws Exception {
-        GoogleAuthenticator gAuth = new GoogleAuthenticator();
-        // 生成一个新的密钥
-        GoogleAuthenticatorKey key = gAuth.createCredentials();
-        String secret = key.getKey();
-        System.out.println("Secret: " + secret);
-
-        // 生成TOTP二维码URL (可以用二维码生成器生成二维码图片)
-        String qrCodeUrl = GoogleAuthenticatorQRGenerator.getOtpAuthURL("MyApp", "user@example.com", key);
-        System.out.println("QR Code URL: " + qrCodeUrl);
-
-        // 生成当前的TOTP代码
-//        int code = gAuth.getTotpPassword(secret);
-        int code = gAuth.getTotpPassword("BRF23ZDOFIBUT6ZCR4EYCQS2ONZJ7HO4");
-        System.out.println("Current OTP: " + code);
-
-        // 验证TOTP代码
-        boolean isCodeValid = gAuth.authorize("BRF23ZDOFIBUT6ZCR4EYCQS2ONZJ7HO4", code);
-        System.out.println("Is code valid? " + isCodeValid);
-
-//        System.out.println("Current OTP: " + generateQRCodeBase64("ALBAGBDBKPWUQUGL"));
-//        System.out.println("Current OTP: " + generateQRCodeBase64("otpauth%3A%2F%2Ftotp%2FMyApp%3Auser%40example.com%3Fsecret%3D6XCO4T77RYHYC62T%26issuer%3DMyApp%26algorithm%3DSHA1%26digits%3D6%26period%3D30&size=200x200&ecc=M&margin=0"));
-        String totpUri = "otpauth://totp/MyApp:user@example.com?secret=BRF23ZDOFIBUT6ZCR4EYCQS2ONZJ7HO4&issuer=MyApp&algorithm=SHA1&digits=6&period=30";
-        int size = 200;
-
-        try {
-            String base64QRCode = GoogleAuthenticatorQRGenerator.getOtpAuthTotpURLBase64(totpUri, size,size);
-            System.out.println("Base64 QR code: " + base64QRCode);
-        } catch (Exception e) {
-            System.err.println("Could not generate QR code: " + e.getMessage());
-        }
-    }
-
-
-}
+//package com.frame.template.service.system.controller;
+//
+//
+//import com.gstdev.cloud.authenticator.qr.google.GoogleAuthenticator;
+//import com.gstdev.cloud.authenticator.qr.google.GoogleAuthenticatorKey;
+//import com.gstdev.cloud.authenticator.qr.google.GoogleAuthenticatorQRGenerator;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/mfa")
+//public class GoogleMFA {
+//
+//
+//    public static void main(String[] args) throws Exception {
+//        GoogleAuthenticator gAuth = new GoogleAuthenticator();
+//        // 生成一个新的密钥
+//        GoogleAuthenticatorKey key = gAuth.createCredentials();
+//        String secret = key.getKey();
+//        System.out.println("Secret: " + secret);
+//
+//        // 生成TOTP二维码URL (可以用二维码生成器生成二维码图片)
+//        String qrCodeUrl = GoogleAuthenticatorQRGenerator.getOtpAuthURL("MyApp", "user@example.com", key);
+//        System.out.println("QR Code URL: " + qrCodeUrl);
+//
+//        // 生成当前的TOTP代码
+////        int code = gAuth.getTotpPassword(secret);
+//        int code = gAuth.getTotpPassword("BRF23ZDOFIBUT6ZCR4EYCQS2ONZJ7HO4");
+//        System.out.println("Current OTP: " + code);
+//
+//        // 验证TOTP代码
+//        boolean isCodeValid = gAuth.authorize("BRF23ZDOFIBUT6ZCR4EYCQS2ONZJ7HO4", code);
+//        System.out.println("Is code valid? " + isCodeValid);
+//
+////        System.out.println("Current OTP: " + generateQRCodeBase64("ALBAGBDBKPWUQUGL"));
+////        System.out.println("Current OTP: " + generateQRCodeBase64("otpauth%3A%2F%2Ftotp%2FMyApp%3Auser%40example.com%3Fsecret%3D6XCO4T77RYHYC62T%26issuer%3DMyApp%26algorithm%3DSHA1%26digits%3D6%26period%3D30&size=200x200&ecc=M&margin=0"));
+//        String totpUri = "otpauth://totp/MyApp:user@example.com?secret=BRF23ZDOFIBUT6ZCR4EYCQS2ONZJ7HO4&issuer=MyApp&algorithm=SHA1&digits=6&period=30";
+//        int size = 200;
+//
+//        try {
+//            String base64QRCode = GoogleAuthenticatorQRGenerator.getOtpAuthTotpURLBase64(totpUri, size,size);
+//            System.out.println("Base64 QR code: " + base64QRCode);
+//        } catch (Exception e) {
+//            System.err.println("Could not generate QR code: " + e.getMessage());
+//        }
+//    }
+//
+//
+//}
--- frame-template-services/frame-template-service-system/src/main/java/com/frame/template/service/system/controller/StripeController.java
+++ frame-template-services/frame-template-service-system/src/main/java/com/frame/template/service/system/controller/StripeController.java
@@ -1,139 +1,44 @@
-package com.frame.template.service.system.controller;
-
-import com.gstdev.cloud.base.core.utils.SecureUtil;
-import com.gstdev.cloud.base.definition.domain.Result;
-import com.gstdev.cloud.oauth2.core.utils.SecurityUtils;
-import com.gstdev.cloud.service.system.domain.pojo.sysPermission.PermissionManageQO;
-import com.stripe.exception.StripeException;
-import io.swagger.v3.oas.annotations.Operation;
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.Resource;
-import org.springframework.core.env.Environment;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-
-@RestController
-@RequestMapping("/aaaaaaaat")
-public class StripeController {
-    @Resource
-    Environment environment;
-    @PostConstruct
-    public void postConstruct() {
-        Environment environment1 = environment;
-        String property = environment.getProperty("gstdev.cloud.swagger.enabled");
-        System.out.println(property);
-        System.out.println("aaaaaaaaa11112");
-    }
-    @GetMapping("/a")
-    @Operation(summary = "a")
-    public void a() {
-        System.out.println("aaaaaaaaa1111");
-        SecurityContext securityContext = SecurityUtils.getSecurityContext();
-        System.out.println(SecurityUtils.getTokenValue());
-    }
-    @PostMapping("/{aaa}/get-permission-manage-page")
-    @Operation(summary = "get-permission-manage-page")
-    public Result<Map<String, Object>> getPermissionManagePage(@RequestBody PermissionManageQO permissionManageQO) {
-        System.out.println("aaaaaaaaa1111");
-        SecurityContext securityContext = SecurityUtils.getSecurityContext();
-        System.out.println(SecurityUtils.getTokenValue());
-        return null;
-    }
-
-    public static String generateKey(List<String> input) {
-// 对字符串列表进行排序
-        Collections.sort(input);
-
-// 连接排序后的字符串
-        String combinedInput = String.join("", input);
-//        System.out.println(combinedInput);
-        return SecureUtil.md5(combinedInput);
-    }
-//    [0]
-//            [0, 2]
-//            [0, 3]
-//            [0, 1, 2]
-//            [0, 1, 2, 3]
-//            [1]
-//            [1, 2]
-//            [1, 3]
-//            [1, 2, 3]
-//            [2]
-//            [2, 3]
-public static void generateCombinations(List<String> input, List<String> tempCombination, int start) {
-    for (int i = start; i < input.size(); i++) {
-        tempCombination.add(input.get(i));
-        System.out.println(tempCombination);
-        generateCombinations(input, tempCombination, i + 1);
-        tempCombination.remove(tempCombination.size() - 1);
-    }
-}
-
-
-    public static List<String> printCombinations(List<String> array, int start, ArrayList<String> prefix) {
-        ArrayList<String> objects = new ArrayList<>();
-        objects.add(generateKey(prefix));
-        String s = generateKey(prefix);
-        System.out.println("aaaaaaaaaaaa"+prefix);
-        for (int i = start; i < array.size(); i++) {
-            ArrayList<String> newPrefix = new ArrayList<>(prefix);
-            newPrefix.add(array.get(i));
-            objects.addAll(printCombinations(array, i + 1, newPrefix));
-        }
-        return objects;
-    }
-
-
-    public static void main(String[] args) throws StripeException {
-        List<String> combinedCodes = new ArrayList<>();
-        for (int i = 0; i < 30; i++) {
-            combinedCodes.add(String.valueOf(i));
-        }
-        List<String> strings = printCombinations(combinedCodes, 0, new ArrayList<>());
-        System.out.println(strings);
-//        List<String> combinedCodes = new ArrayList<>();
-//        List<String> tempCombination1 = new ArrayList<>();
-//        int a = 0;
-//        for (int i = 0; i < 4; i++) {
-//            List<String> tempCombination = new ArrayList<>();
-//            System.out.println(i);
-//            for (int j = 0; j < 4; j++) {
-//                tempCombination.add(String.valueOf(j));
-//                System.out.println(tempCombination);
-//                a++;
-//            }
-//        }
-//        System.out.println(a);
-//List<String> combinedCodes = new ArrayList<>();
-//combinedCodes.add("gstdev-systemput:v1:security:reset-password:originalPassword:newPassword");
-//combinedCodes.add("gstdev-systemput:v1:security:update-account-current-login-information");
-////combinedCodes.add("gstdev-systemget:v1:account:get-account-settings-detail");
-////combinedCodes.add("gstdev-systemget:v1:user:get-user-settings-detail");
-////combinedCodes.add("gstdev-systemget:v1:system:constant:enums");
-////combinedCodes.add("gstdev-systempost:v1:account:update-account-settings-detail");
-//String key2 = generateKey(combinedCodes);
-//System.out.println(key2);
-//Stripe.apiKey = "sk_test_51PNQG8Hsb9kAuba1PyOGdmhpyuR17Y0Gqq3OrjWzYxQSeFrfZyVO33akMLPZMA9PmQhsHSkyHcN4PSE3qZwFAgF700dRCVuq76";
+//package com.frame.template.service.system.controller;
 //
-//SessionCreateParams params =
-//SessionCreateParams.builder()
-//.setSuccessUrl("https://example.com/success")
-//.addLineItem(
-//SessionCreateParams.LineItem.builder()
-//.setPrice("price_1MotwRLkdIwHu7ixYcPLm5uZ")
-//.setQuantity(2L)
-//.build()
-//)
-//.setMode(SessionCreateParams.Mode.PAYMENT)
-//.build();
+//import com.gstdev.cloud.base.definition.domain.Result;
+//import com.gstdev.cloud.oauth2.core.utils.SecurityUtils;
+//import com.gstdev.cloud.service.system.domain.pojo.sysPermission.PermissionManageQO;
+//import io.swagger.v3.oas.annotations.Operation;
+//import jakarta.annotation.PostConstruct;
+//import jakarta.annotation.Resource;
+//import org.springframework.core.env.Environment;
+//import org.springframework.security.core.context.SecurityContext;
+//import org.springframework.web.bind.annotation.*;
 //
-//Session session = Session.create(params);
-//System.out.println("Hello, World!");
-    }
-}
-
+//import java.util.Map;
+//
+//@RestController
+//@RequestMapping("/aaaaaaaat")
+//public class StripeController {
+//    @Resource
+//    Environment environment;
+//    @PostConstruct
+//    public void postConstruct() {
+//        Environment environment1 = environment;
+//        String property = environment.getProperty("gstdev.cloud.swagger.enabled");
+//        System.out.println(property);
+//        System.out.println("aaaaaaaaa11112");
+//    }
+//    @GetMapping("/a")
+//    @Operation(summary = "a")
+//    public void a() {
+//        System.out.println("aaaaaaaaa1111");
+//        SecurityContext securityContext = SecurityUtils.getSecurityContext();
+//        System.out.println(SecurityUtils.getTokenValue());
+//    }
+//    @PostMapping("/{aaa}/get-permission-manage-page")
+//    @Operation(summary = "get-permission-manage-page")
+//    public Result<Map<String, Object>> getPermissionManagePage(@RequestBody PermissionManageQO permissionManageQO) {
+//        System.out.println("aaaaaaaaa1111");
+//        SecurityContext securityContext = SecurityUtils.getSecurityContext();
+//        System.out.println(SecurityUtils.getTokenValue());
+//        return null;
+//    }
+//
+//}
+//
