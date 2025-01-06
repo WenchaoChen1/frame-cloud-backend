6571071074a80a87d4d32277593d21e729de70f5
wenchao <wenchao@goldensection.com>
2025-01-06 11:21:29 +0800

    frame-template-common-删除无用代码

--- frame-template-common/src/main/java/com/frame/template/common/constant/ServletConstants.java
+++
@@ -1,6 +0,0 @@
-package com.frame.template.common.constant;
-
-public interface ServletConstants {
-
-    String AUTHORIZATION = "Authorization";
-}
--- frame-template-common/src/main/java/com/frame/template/common/utils/number/Identifier.java
+++
@@ -1,9 +0,0 @@
-package com.frame.template.common.utils.number;
-
-public interface Identifier {
-    public int getMachine();
-
-    public void setMachine(int machine);
-
-    public long get();
-}
