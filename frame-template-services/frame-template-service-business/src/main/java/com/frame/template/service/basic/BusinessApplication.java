82c37bac3640bac59dd82774857aa14fcd89a11c
wubin <1356526706@qq.com>
2025-01-10 16:53:11 +0800

    删除多余的配置类

--- frame-template-services/frame-template-service-business/src/main/java/com/frame/template/service/basic/BusinessApplication.java
+++ frame-template-services/frame-template-service-business/src/main/java/com/frame/template/service/basic/BusinessApplication.java
@@ -1,13 +1,23 @@
 package com.frame.template.service.basic;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 /**
  * Hello world!
  *
  */
+@Configuration(proxyBeanMethods = false)
+@EntityScan(basePackages = {
+    "com.frame.template.service.basic.domain.entity",
+    "com.frame.template.service.basic.domain.service",
+})
+@EnableJpaRepositories(basePackages = {
+    "com.frame.template.service.basic.repository",
+})
 @SpringBootApplication
 public class BusinessApplication
 {
--- frame-template-services/frame-template-service-business/src/main/java/com/frame/template/service/basic/service/MusicianServiceImpl.java
+++ frame-template-services/frame-template-service-business/src/main/java/com/frame/template/service/basic/service/MusicianServiceImpl.java
@@ -3,15 +3,16 @@ package com.frame.template.service.basic.service;
 import com.frame.template.service.basic.domain.entity.Musician;
 import com.frame.template.service.basic.repository.MusicianRepository;
 import com.gstdev.cloud.data.core.service.BaseServiceImpl;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Transactional(readOnly = true)
+@Service
 public class MusicianServiceImpl extends BaseServiceImpl<Musician, String, MusicianRepository> implements MusicianService {
-
     public MusicianServiceImpl(MusicianRepository musicianRepository) {
         super(musicianRepository);
     }
-
 }
